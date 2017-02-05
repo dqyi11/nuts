c1e6c47a3e104fee9b40bed007c1c5108a6c7db3
Daqing Yi <dqyi11@gmail.com>
2017-02-05 00:40:26 -0500

    x

--- hmc.py
+++ hmc.py
@@ -6,51 +6,45 @@ Created on Feb 2, 2017
 
 import numpy as np
 
-def LeapFrog(x, q, eps, dU, dK):
+def LeapFrog(x, q, eps, dU):
 
-    new_q = q - (eps/2) * dU( x ) 
-    new_x = x + (eps/2) * dK( new_q )
-    new_q = new_q - (eps/2) * dK( new_x )
+    new_q = q + 0.5 * eps * dU( x ) 
+    new_x = x + 0.5 * eps * new_q
+    new_q = new_q + 0.5 * eps * dU( new_x )
     
     return new_x, new_q
 
-def HMC(x0, eps, L, M, U, K, dU, dK):
+def HMC(x0, eps, L, M, U, dU):
     
     dim = len(x0)
-    X = np.zeros((dim, M+1))
-    X = np.matrix(X)
-    X[:,0] = x0
+    X = np.zeros((M+1, dim))
+    X[0, :] = x0
     for m in range(M):
-        q = np.random.randn(dim,1)
-        x = X[:, m]
-        U0 = U(x)
-        K0 = K(q)
+        q = np.random.randn(dim)
+        x = X[m, :]
+        H0 = U(x) - 0.5 *  np.dot(q, q.T)
         for i in range(L):
-            x, q = LeapFrog(x, q, eps, dU, dK)
-        U1 = U(x)
-        K1 = K(q)  
+            x, q = LeapFrog(x, q, eps, dU)
+        H1 = U(x) - 0.5 *  np.dot(q, q.T) 
             
-        alfa = np.min([1.0, np.exp((U0+K0)-(U1+K1))])
         u = np.random.rand()
-        if u <= alfa:
-            X[:,m+1] = x
+        if np.log(u) <= np.min([0.0, H1-H0]):
+            X[m+1,:] = x
         else:
-            X[:,m+1] = X[:,m]       
+            X[m+1,:] = X[m,:]       
     
     return X
 
-def HMC_DA(x0, delta, lam,  M, U, K, dU, dK, M_adapt=None):
+def HMC_DA(x0, delta, lam,  M, U, dU, M_adapt=None):
     
     if M_adapt == None:
         M_adapt = int(M/2)
         
-    eps0 = FindReasonableEpsilon(x0, dU, dK)
+    eps0 = FindReasonableEpsilon(x0, U, dU)
     dim = len(x0)
-    X = np.zeros((dim, M+1))
-    X = np.matrix(X)
+    X = np.zeros((M+1, dim))
+    X[0, :] = x0
     Eps = np.zeros(M+1)
-    Eps = np.matrix(Eps)
-    X[:,0] = x0
     Eps[0] = eps0
     
     mu = np.log(10*eps0)
@@ -61,47 +55,47 @@ def HMC_DA(x0, delta, lam,  M, U, K, dU, dK, M_adapt=None):
     k = 0.75
     
     for m in range(M):
-        q = np.random.randn(dim,1)
-        x = X[:, m]
-        U0 = U(x)
-        K0 = K(q)
+        q = np.random.randn(dim)
+        x = X[m,:]
+        H0 = U(x) - 0.5 *  np.dot(q, q.T)
+        
         L = np.max([1, int( lam / Eps[m] )])
         for i in range(L):
-            x, q = LeapFrog(x, q, Eps[m], dU, dK)
-        U1 = U(x)
-        K1 = K(q)  
+            x, q = LeapFrog(x, q, Eps[m], dU)
+        H1 = U(x) - 0.5 *  np.dot(q, q.T)
             
-        alfa = np.min([1.0, np.exp((U0+K0)-(U1+K1))])
         u = np.random.rand()
-        if u <= alfa:
-            X[:,m+1] = x
+        if np.log(u) <= np.min([0.0, H1-H0]):
+            X[m+1,:] = x
         else:
-            X[:,m+1] = X[:,m]       
+            X[m+1,:] = X[m,:]       
     
         if m <= M_adapt:
-            H_b = (1 - 1/(m+t0)) * H_b + (1/(m+t0)) * (delta - alfa)
+            alfa = np.exp(H1-H0)
+            eta = 1./float(m+t0)
+            H_b = (1 - eta) * H_b + eta * (delta - alfa)
             Eps[m+1] = np.exp( mu - np.sqrt(m) / gamma * H_b )
-            eps_b = np.exp( m**(-k) * np.log( Eps[m+1] ) + (1 - m**(-k)) * np.log( eps_b ) )
+            eta = (m+1)**(-k) # m start from 0
+            eps_b = np.exp( eta * np.log( Eps[m+1] ) + (1. - eta) * np.log( eps_b ) )
         else:
             Eps[m+1] = eps_b 
-        
+            
     return X
     
-def FindReasonableEpsilon(x, dU, dK):
+def FindReasonableEpsilon(x, U, dU):
     eps = 1.0
     dim = len(x)
-    q = np.random.randn(dim,1)
-    new_x, new_q = LeapFrog(x, q, eps, dU, dK) 
-    log_prob = 0.0
-    new_log_prob = 0.0
-    if new_log_prob - log_prob > np.log(0.5):
-        a = 1
-    else:
-        a = -1
+    q = np.random.randn(dim)
+    
+    log_prob = U(x) - 0.5 * np.dot(q, q.T)   
+    new_x, new_q = LeapFrog(x, q, eps, dU) 
+    new_log_prob = U(new_x) - 0.5 * np.dot(new_q, new_q.T)
+    a = 2. * float( new_log_prob - log_prob > np.log(0.5) ) - 1
+    
     while a * (new_log_prob - log_prob) > - a * np.log(2):
         eps = 2.0**2 * eps
-        new_x, new_q = LeapFrog(x, q, eps, dU, dK) 
-        log_prob = 0.0
-        new_log_prob = 0.0
+        new_x, new_q = LeapFrog(x, q, eps, dU) 
+        log_prob = U(x) - 0.5 * np.dot(q, q.T)
+        new_log_prob = U(new_x) - 0.5 * np.dot(new_q, new_q.T)
         x , q = new_x, new_q
     return eps    
--- m1.py
+++ m1.py
@@ -13,48 +13,47 @@ if __name__ == '__main__':
     
     # step size
     eps = 0.3
-    sample_num = 500
+    sample_num = 1000
     L = 20
     
     mu = np.zeros((2,1))
-    var = np.matrix([[1, .8],[.8, 1]])
-    M = np.matrix([[1,0],[0,1]])
+    #var = np.matrix([[1, .8],[.8, 1]])
+    cov = np.asarray([[1, .8],[.8, 4]])
 
     # potential energy function
     def U_func(X):    
-        U = X.T * np.linalg.inv(var) * X / 2
-        return np.sum(U)
+        tmp = - np.dot(X, np.linalg.inv(cov))
+        U = 0.5 * np.dot( tmp, X.T )
+        return U
     
     # gradient potential energy function
     def dU_func(X):
-        A = np.linalg.inv(var) * X
-        return A
+        grad = - np.dot(X, np.linalg.inv(cov))
+        return grad
         
-    # kinetic energy function    
-    def K_func(P):
-        s = np.sum( P.T * M * P / 2 )
-        return s
+    x0 = np.array([0,6])
     
-    # gradient kinetic energy function
-    def dK_func(P):
-        return M * P
+    X = HMC(x0, eps, L, sample_num, U_func, dU_func)
     
-    x0 = np.matrix([[0],[6]])
+    delta = 0.2
+    lam = 1.0
+    X = HMC_DA(x0, delta, lam, sample_num, U_func, dU_func)
     
-    X = HMC(x0, eps, L, sample_num, U_func, K_func, dU_func, dK_func)
-    X = HMC_DA(x0, eps, lam, sample_num, U_func, K_func, dU_func, dK_func)
-    
-    s_mu = np.mean(X,1)
-    s_var = np.cov(X)
-    
+    s_mu = np.mean(X,0)
+    s_cov = np.cov(X,rowvar=False)
     print s_mu
-    print s_var
-            
+    print s_cov
+    
+    temp = np.random.multivariate_normal(mu[:,0], cov, size=sample_num)
+    #print temp
+           
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.scatter(X[0,:],X[1,:])
+    ax.plot(X[:,0],X[:,1],'b.')
+    ax.plot(temp[:,0],temp[:,1], 'r+')
     #ax.plot(X[0,0:50],X[1,0:50],'r')
     #ax.set_xlim([-6,6])
     #ax.set_ylim([-6,6])
+    
     
     plt.show()
